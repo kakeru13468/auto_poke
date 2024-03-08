0d047ed9a6086b65e8f7cd20fc159ca734e8a09f
Li-Ching <66195059+Li-Ching@users.noreply.github.com>
2024-03-08 19:42:59 +0800

    update

--- auto_poke.py
+++ auto_poke.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 count = 10
 driver = webdriver.Edge()
 driver.get("https://www.facebook.com/pokes")
